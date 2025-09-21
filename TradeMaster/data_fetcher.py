795abf8cc218abcc010f8191bd6797dd91e66217
Sumeet Deshpande <sumeetdeshpande003@gmail.com>
2025-09-21 11:51:07 +0530

    updates

--- TradeMaster/data_fetcher.py
+++
@@ -1,7 +0,0 @@
-from .options.options_datafetcher import OptionsDataFetcher
-from questdb_query import pandas_query, Endpoint, numpy_query
-
-
-host = '62.72.42.9'
-endpoint = Endpoint(host=host, port=9000, https=False, username='admin', password='2Cents#101')
-Options_Data_Fetcher = OptionsDataFetcher(endpoint=endpoint)
